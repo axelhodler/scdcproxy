0c025f595065edf5c448068910048d3bc8f009ad
Axel Hodler <axel@hodler.co>
2018-04-12 11:44:46 +0100

    after dl from start.spring.io

---
+++ src/main/java/com/hodler/scdcproxy/ScdcproxyApplication.java
@@ -0,0 +1,12 @@
+package com.hodler.scdcproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScdcproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScdcproxyApplication.class, args);
+	}
+}
