c89c8f2e2e27908fdf5c913eaab9a71ff29306a0
Axel Hodler <axel@hodler.co>
2018-05-06 19:04:38 +0200

    first example

---
+++ src/test/resources/contracts/shouldProvideDistanceBetweenTwoCities.groovy
@@ -0,0 +1,25 @@
+org.springframework.cloud.contract.spec.Contract.make {
+    request {
+        method GET()
+
+        url("/maps/api/distancematrix/json") {
+            queryParameters {
+                parameter 'origins': 'Berlin'
+                parameter 'destinations': 'Stuttgart'
+            }
+        }
+    }
+
+    response {
+        status 200
+        body([
+            rows: [[
+                       elements: [[
+                                      duration: [
+                                          value: 22738
+                                      ]
+                                  ]]
+                   ]]
+        ])
+    }
+}
